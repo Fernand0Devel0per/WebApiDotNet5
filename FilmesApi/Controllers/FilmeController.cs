dc15e684e7e4287b5f55b43f888f7efd394c2b7d
Fernando Henrique Lotti <fernand0devel0per@hotmail.com>
2022-08-31 16:00:56 -0300

    first commit

---
+++ FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,19 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        public void AddFilme(Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine();
+        }
+    }
+}
